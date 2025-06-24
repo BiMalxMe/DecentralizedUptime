2597240db690ba82b3b1f73495ab326e4106b9a4
BiMalxMe <journeyxbimal@gmail.com>
2025-06-24 12:33:11 +0545

    MainHooksForRefreshingAndGettingALltheWebistesOnLogin

--- apps/web/components/hooks/useWebsites.tsx
+++ apps/web/components/hooks/useWebsites.tsx
@@ -1,8 +1,44 @@
 import { useAuth } from "@clerk/nextjs";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import {Api_Backend_Url} from "@/config"
+
+interface Website{
+    id : string,
+    url : string,
+    ticks : {
+        id: string,
+        createdAt : string,
+        status : string,
+        latency : number
+    }[]
+}
 
 export function useWebsites(){
     const {getToken} = useAuth()
+    const [websites,setwebsites ] = useState<Website[]>([]);
+
+    async function refreshWebsites(){
+        const token = await getToken();
+        const response = await axios.get(`${Api_Backend_Url}/api/v1/website`,{
+            headers :{
+                Authorization : token
+            }
+        })
+        setwebsites(response.data.websites)
+    }
 
 
 
+useEffect(() => {
+    refreshWebsites()
+
+    const interval = setInterval(() => {
+        refreshWebsites();
+    },1000 * 60 * 1);
+
+    return ()=> clearInterval(interval)
+},[])
+
+return { websites,refreshWebsites}
 }
